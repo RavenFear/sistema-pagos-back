4ebd58b901aa6cfb3f0d9b27ab18e4bb71b005a5
RavenFear <100720205+RavenFear@users.noreply.github.com>
2025-05-26 14:10:15 -0500

    Ajustes proyecto.

--- src/main/java/com/systempaymentsunisalle/sistema_pagos_backend_unisalle/SistemaPagosBackendUnisalleApplication.java
+++ src/main/java/com/systempaymentsunisalle/sistema_pagos_backend_unisalle/SistemaPagosBackendUnisalleApplication.java
@@ -27,54 +27,6 @@ public class SistemaPagosBackendUnisalleApplication {
     CommandLineRunner commandLineRuuner(EstudianteRepository estudianteRepository, PagoRepository pagoRepository) {
         return args -> {
 
-            estudianteRepository.save(Estudiante.builder()
-                    .id(UUID.randomUUID().toString()) //Genera un identificador único aleatorio para el estudiante
-                    .nombre("Christian") //Nombre del estudiante
-                    .apellido("Ramirez") //Apellido del estudiante
-                    .codigo("1234") //Codigo del estudiante en la universidad
-                    .programaId("LTA1") //Programa academico al que pertenece
-                    .build());
-
-            //Agrega otro estudiante llamado "Biaggio Ramirez" al mismo programa
-            estudianteRepository.save(Estudiante.builder()
-                    .id(UUID.randomUUID().toString())
-                    .nombre("Biaggio")
-                    .apellido("Ramirez")
-                    .codigo("12354")
-                    .programaId("LTA1")
-                    .build());
-
-            //Agrega un tercer estusiante llamado "Valentina Lopez" en otro programa ("LTA2")
-            estudianteRepository.save(Estudiante.builder()
-                    .id(UUID.randomUUID().toString())
-                    .nombre("Valentina")
-                    .apellido("Lopez")
-                    .codigo("5678")
-                    .programaId("LTA2")
-                    .build());
-
-
-            TypePago[] tiposPago = TypePago.values();
-
-            Random random = new Random();
-
-            estudianteRepository.findAll().forEach(estudiante -> {
-
-                for (int i = 0; i < 10; i++) {
-                    int index = random.nextInt(tiposPago.length);
-
-                    Pago pago = Pago.builder()
-                            .monto(1000 + (int) (Math.random() * 20000)) //Genera un monto entre 1000 y 21000
-                            .type(tiposPago[index]) //Asigna un tipo de pago aleatorio
-                            .status(StatusPago.CREADO) //Estado inicial del pago (CREADO)
-                            .fecha(LocalDate.now()) //Fecha actual del Pago
-                            .estudiante(estudiante) //Asigna el Pago al estudiante actual
-                            .build();
-
-
-                    pagoRepository.save(pago);
-                }
-            });
         };
 
     }
